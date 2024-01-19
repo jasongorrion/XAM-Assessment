7073229cde2d56855cebe8c16dd3240dc12fb5dd
Jason Gorrion <jason.gorrion@gmail.com>
2024-01-19 10:03:53 +0800

    Animation Setup

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 (function() {
+    //On scroll Animation
+    let offset = 700;//Distance from Top before trigger animation
+    let element_with_animation = $('.animated'); //Elements with animation
+
+    updateElementsVisibility();
+
+    //On scroll events
+    $(document).on('scroll', function() {
+        //Moving Text
+        let scrollPosition = $(document).scrollTop();
+        let moveToLeft = (scrollPosition/5) * -1;
+        let translationToLeft = 'translateX(' + moveToLeft + 'px)';
+        let moveToRight = scrollPosition/5;
+        let translationToRight = 'translateX(' + moveToRight + 'px)';
+        $('.moving_text_col_wrapper:first').css('transform', translationToLeft);
+        $('.moving_text_col_wrapper:eq(1)').css('transform', translationToRight);
+        $('.moving_text_col_wrapper:eq(2)').css('transform', translationToLeft);
+        //End
+
+        updateElementsVisibility();
+    });
+
+    function updateElementsVisibility(){
+        let scrollPosition = $(document).scrollTop();
+
+        //On scroll animation / entrance animations
+        // Loop through each element
+        element_with_animation.each(function() {
+            // Get the top position of the current element
+            var elementTop = $(this).offset().top;
+            console.log(elementTop - offset <= scrollPosition);
+            // Check if the element is near the top
+            if (elementTop - offset <= scrollPosition) {
+                // Add the class to the current element
+                $(this).addClass('active');
+            } else {
+                // Remove the class if the element is not near the top
+                $(this).removeClass('active');
+            }
+        });
+    }
+    
     //CASE STUDIES SLIDER INTIALIZATION
     $('.case_studies_slider').slick({
         slidesToShow:3,
@@ -10,6 +52,7 @@
         centerPadding: '0px'
     });
 
+    //ACCORDION EVENT
     $('.accordion_header').click(function(){
         if($(this).hasClass('active_accordion')){
             $(this).find('.accordion_content').hide();
@@ -23,6 +66,5 @@
             $(this).find('.accordion_content').fadeIn();
             $(this).find('.accordion_icon').text('-');
         }
-        
     });
 })();
